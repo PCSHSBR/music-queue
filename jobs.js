5038128201faa37d78c8ef26437dcfe4bcd9b9ba
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2022-12-06 00:27:29 +0700

    add clear sheet script

--- jobs.js
+++ jobs.js
@@ -6,4 +6,25 @@ function deleteOldSongsInForm() {
   Logger.log("Deleting old songs in form");
   let activeForm = FormApp.getActiveForm();
   activeForm.deleteAllResponses();
+  resetFormTargetSheet()
+}
+
+function resetFormTargetSheet() {
+  Logger.log("Resetting form target sheet");
+  // get current sheet target by active google form
+  let activeForm = FormApp.getActiveForm();
+  let formTargetSheetID = activeForm.getDestinationId();
+  if (formTargetSheetID) {
+    let sheet = SpreadsheetApp.openById(formTargetSheetID);
+    // form target sheet usually the first sheet
+    let sheetData = sheet.getSheets()[0];
+    // clear all data in sheet except header in first row
+    sheetData.getRangeList(["A2:D"]).clearContent().clearFormat().clearNote();
+  } else {
+    // target spreadsheet not found
+    Logger.log("Target spreadsheet not found");
+    // create new and set as form target
+    let newSheet = SpreadsheetApp.create(`Song request (from ${Utilities.formatDate(new Date(), "GMT+7", "yyyy-MM-dd")})`);
+    activeForm.setDestination(FormApp.DestinationType.SPREADSHEET, newSheet.getId());
+  }
 }
