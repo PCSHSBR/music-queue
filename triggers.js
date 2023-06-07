0947534bf8452002bf0e3cac22d4e51459ffa8ec
Patsagorn <patsagorn_y@outlook.com>
2023-06-07 10:08:15 +0700

    feat(trigger): use lock service
    to prevent append colision in Google form incase of high amont of
    request in the same minute, which resulte in overwrite the same new row.

--- triggers.js
+++ triggers.js
@@ -4,13 +4,15 @@ function setup() {
   SpreadsheetApp.getActiveSpreadsheet();
 }
 
-// on form submit
-function onFormSubmit(e) {
-  // Logger.log("onFormSubmit triggered")
-  // append data to sheet
-  let sheet = SpreadsheetApp.openByUrl(DATA_SHEET_URL);
-  let sheetData = sheet.getSheetByName(SHEET_NAME);
+let sheetLocker = LockService.getScriptLock();
 
+// on form submit, save response to data sheet
+function onFormSubmit(e) {
+  let sheet = SpreadsheetApp.openByUrl(DATA_SHEET_URL);
+  let sheetDataSheet = sheet.getSheetByName(SHEET_NAME);
+
+  // `exponentialBackoff_` is needed as the response data might not be available
+  // immediately after onFormSubmit is invoked. Result in item responses are null.
   let data;
   try {
     data = exponentialBackoff_(() => e.response.getItemResponses());
@@ -18,7 +20,10 @@ function onFormSubmit(e) {
     Logger.log(error.message);
     data = "";
   }
+
+  // Init row array
   let row = [];
+  // Use `exponentialBackoff_` same reason as above.
   let date;
   try {
     date = exponentialBackoff_(() => e.response.getTimestamp());
@@ -26,11 +31,31 @@ function onFormSubmit(e) {
     Logger.log(error.message);
     date = "";
   }
+  // Give a precise time of request.
   // 2001-07-04T12:08:56.235-0700
   row.push(Utilities.formatDate(date, "GMT+7", "yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
+
+  // Loop all response field, save it to an array.
   for (let i = 0; i < data.length; i++) {
     row.push(data[i].getResponse());
   }
+
+  // For inspecting reason.
   Logger.log(row);
-  sheetData.appendRow(row);
+  
+  // Next part is soldly for preventing google sheet overwriting data in the same row
+  // when there's a lot of request in form.
+  // It will try to wait as per exponentialBackoff_ work when the lock still active,
+  // then continue again.
+  exponentialBackoff_(function waitLock() {
+    // locking for 5s
+    // no reason for this magic number, just come up in my mind
+    sheetLocker.waitLock(5000);
+  }, 5)
+  
+  // Now actually append new data to the data sheet.
+  sheetDataSheet.appendRow(row);
+
+  // release lock
+  sheetLocker.releaseLock();
 }
